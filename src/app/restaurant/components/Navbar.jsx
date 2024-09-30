1bb38a44b987f3a0951b40c8f3a49d6624e96393
Princy Patel <kapatelprincy@gmail.com>
2024-10-01 00:32:48 +0530

    fix:implement link component

--- src/app/restaurant/components/Navbar.jsx
+++ src/app/restaurant/components/Navbar.jsx
@@ -1,12 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 function Navbar() {
   return (
     <nav className="bg-white p-2 flex justify-between">
-      <a href="" className="font-bold text-gray-700 text-2xl">
-        {" "}
-        OpenTable{" "}
-      </a>
+      <Link href="" className="font-bold text-gray-700 text-2xl">
+        OpenTable
+      </Link>
       <div>
         <div className="flex">
           <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
---
+++ src/app/restaurant/components/RestaurantNavbar.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+function RestaurantNavbar() {
+  return (
+    <nav className="flex text-reg border-b pb-2">
+      <Link href={"/restaurant/cnf"} className="mr-7">
+        Overview
+      </Link>
+      <Link href={"/restaurant/cnf/menu"} className="mr-7">
+        Menu
+      </Link>
+    </nav>
+  );
+}
+
+export default RestaurantNavbar;
