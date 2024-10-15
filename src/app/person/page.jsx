711595282e1da4fe43dd545748d8c7ac4b30cbf6
Princy Patel <kapatelprincy@gmail.com>
2024-10-15 22:27:11 +0530

    fix:mutate from swr

--- src/app/person/page.jsx
+++ src/app/person/page.jsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import React from "react";
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 
 function Person() {
   const fetcher = (url) => fetch(url).then((response) => response.json());
@@ -10,6 +10,8 @@ function Person() {
   const { data, error, isLoading } = useSWR(
     "https://jsonplaceholder.typicode.com/todos",
     fetcher
+    // call the API every 1000ms
+    // { refreshInterval: 1000 }
   );
 
   if (error) {
@@ -20,8 +22,17 @@ function Person() {
     return <div>Loading...</div>;
   }
 
+  // call on every click
+  const handleMutate = () => {
+    mutate("https://jsonplaceholder.typicode.com/todos");
+  }
+
   return (
     <div>
+      <button onClick={handleMutate} className=" bg-[#DC2626] text-white p-1 px-4 rounded">
+        Fetch
+      </button>
+
       {data?.map((item) => (
         <Link key={item.id} href={`/person/${item.id}`}>
           <li>{item.title}</li>
