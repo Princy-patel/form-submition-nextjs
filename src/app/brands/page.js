f2f5db9986079bb0d43c08deebd5f301063db14c
Princy Patel <kapatelprincy@gmail.com>
2024-10-24 23:32:56 +0530

    json server

---
+++ src/app/brands/page.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+async function BrandPage() {
+  const response = await fetch("http://localhost:3000/data", {
+    cache: "no-store",
+  });
+  const result = await response.json();
+  return (
+    <div>
+      {result.map((product) => {
+        return (
+          <div key={product.product_id}>
+            <h1>
+              <b className="text-red-500">Product Name: </b>
+              {product.product_name}
+            </h1>
+            <h3>
+              <p>
+                <b className="text-red-500">Description: </b>
+                {product.product_id}
+              </p>
+            </h3>
+            <p>
+              <b className="text-red-500">Description: </b>
+              {product.description}
+            </p>
+            <br />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default BrandPage;
