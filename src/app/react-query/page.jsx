44eb61ae2f76875ebc586509825cc5afa4ad6f49
Princy Patel <kapatelprincy@gmail.com>
2025-01-05 23:36:47 +0530

    fix:handle pending state

--- src/app/react-query/page.jsx
+++ src/app/react-query/page.jsx
@@ -5,16 +5,33 @@ import { useQuery } from "@tanstack/react-query";
 import React, { useEffect } from "react";
 
 function ReactQueryPage() {
-  const { data } = useQuery({
+  // useQuery: fetches and reads data (get requests) from an API and automatically caches the result
+  // useMutation: used for creating, updating or deleting data (POST, PUT, DELETE requests) and allows triggering manual side effects. 
+  const { data, isPending, isError, error } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchData,
+    refetchOnMount: true,
   });
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return (
+      <div className="text-red-500">
+        {error.message} || Something went wrong
+      </div>
+    );
+  }
 
   return (
     <div>
       <ul>
         {data?.map((data) => (
-          <li className="border p-2 my-2 border-red-500" key={data.id}>{data.title}</li>
+          <li className="border p-2 my-2 border-red-500" key={data.id}>
+            {data.title}
+          </li>
         ))}
       </ul>
     </div>
